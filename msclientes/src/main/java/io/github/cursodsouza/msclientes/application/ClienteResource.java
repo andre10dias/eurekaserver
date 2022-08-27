9c378429c9f4d76ff2900caa9c1383e17dcdab13
andre10dias <andre10dias@ymail.com>
2022-08-27 16:49:14 -0300

    Seção 2 - 10 e 11.Implementando, concluindo e testando microservice de clientes

---
+++ msclientes/src/main/java/io/github/cursodsouza/msclientes/application/ClienteResource.java
@@ -0,0 +1,57 @@
+package io.github.cursodsouza.msclientes.application;
+
+import io.github.cursodsouza.msclientes.application.representation.clienteSaveRequest;
+import io.github.cursodsouza.msclientes.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/clientes")
+@RequiredArgsConstructor
+public class ClienteResource {
+
+    private final ClienteService service;
+
+    @GetMapping
+    public String status() {
+        return "ok";
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf) {
+        Optional<Cliente> cliente = service.getByCpf(cpf);
+        if (cliente.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody clienteSaveRequest request) {
+        Cliente cliente = request.toModel();
+        service.save(cliente);
+
+        /**
+         * ServletUriComponentsBuilder - constrói urls dinâmicas, a partir
+         * da url corrente.
+         *
+         * Exemplo:
+         * url corrente: http://localhost:PORT/
+         * url gerada: http://localhost:PORT/clientes?cpf=01234567890
+        * */
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+}
