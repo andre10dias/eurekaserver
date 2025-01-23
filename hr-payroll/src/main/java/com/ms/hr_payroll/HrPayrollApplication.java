7275ec74641e8163c4a258a0f859005a823e0d7b
andre10dias <andre10dias@ymail.com>
2025-01-23 17:50:22 -0300

    Spring cloud load balance

--- hr-payroll/src/main/java/com/ms/hr_payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/ms/hr_payroll/HrPayrollApplication.java
@@ -2,8 +2,12 @@ package com.ms.hr_payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import com.ms.hr_payroll.config.CustomLoadBalancerConfig;
+
+@LoadBalancerClients(defaultConfiguration = CustomLoadBalancerConfig.class)
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
