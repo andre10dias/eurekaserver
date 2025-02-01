ca37be02715752f7894a4a0db64794f4305b57e5
andre10dias <andre10dias@ymail.com>
2025-02-01 19:22:52 -0300

    Configuring Webflux api gateway

--- hr-api-gateway-zuul/src/main/java/com/ms/hr_api_gateway_zuul/HrApiGatewayWebfluxApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/ms/hr_api_gateway_zuul/HrApiGatewayWebfluxApplication.java
@@ -2,12 +2,14 @@ package com.ms.hr_api_gateway_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class HrApiGatewayZuulApplication {
+@EnableDiscoveryClient
+public class HrApiGatewayWebfluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+		SpringApplication.run(HrApiGatewayWebfluxApplication.class, args);
 	}
 
 }
